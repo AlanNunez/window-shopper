812e79e31c64e0e10aac115de2150430c63fbc56
Alan Nunez <nunear@icloud.com>
2018-10-09 03:05:27 -0400

    Using custom textfields

---
+++ Views/CurrencyTextField.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Alan Nunez on 10/9/18.
+//  Copyright © 2018 Alan Nunez. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9423211231, green: 0.9875946247, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
