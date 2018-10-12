731d3a49d1bbc23d6647cd8a956f018d39e5cbf2
Alan Nunez <nunear@icloud.com>
2018-10-12 17:20:25 -0400

    Updating viewController and Storyboard

--- Controller/MainViewController.swift
+++ Controller/MainViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var logoCompany: UIImageView!
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
--- Views/CurrencyTextField.swift
+++ Views/CurrencyTextField.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 0.9423211231, green: 0.9875946247, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -19,6 +28,6 @@ class CurrencyTextField: UITextField {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        }
     }
+  }
 }
