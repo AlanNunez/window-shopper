60c757ccb4487c3c2b4313060b208cee61cc6e56
Wilson Garcia <wilson.garcia@verizon.com>
2018-12-31 12:02:51 -0400

    Adding costum currency sign

--- Views/CurrencyTextField.swift
+++ Views/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2 ) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7843222813, green: 0.8492028736, blue: 0.943458258, alpha: 0.7991224315)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -23,6 +38,8 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 0.9423211231, green: 0.9875946247, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
