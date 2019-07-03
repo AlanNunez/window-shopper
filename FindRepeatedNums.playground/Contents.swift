be5dff1aa9fc50d05850ee8c2efe5accbb30b6cd
Alan Nunez <raul.nunez01@gmail.com>
2019-07-03 14:25:48 -0400

    Finding minimal value in array

--- FindRepeatedNums.playground/Contents.swift
+++ FindRepeatedNums.playground/Contents.swift
@@ -3,12 +3,11 @@ import UIKit
 var numsOne = [1, 3, 4, 5, 6, 7]
 var numsTwo = [1, 3, 5, 6, 6, 7]
 var numsThree = [1, 3, 3, 5, 6, 1, 3, 3, 3, 5, 6, 7]
-var numsFour = [1, 2, 2, 5, 6, 1, 1, 3, 3, 5, 6, 7]
+var numsFour = [1, 2, 1, 3, 2, 1]
 
 var setOfNumbs = Set<Int>()
-var dicOfNumbs = Dictionary<Int, Int>()
+var dicOfNumbs = Dictionary<Int, (Int, Bool)>()
 var min = Int.max
-var key = 0
 var temp = -1
 var disrupt = Int.min
 
@@ -17,21 +16,13 @@ func getRepeatedValue(values: inout [Int]) -> Int {
 for (index, num) in values.enumerated() {
     
     setOfNumbs.insert(num)
-    
-    if let repeated = dicOfNumbs[num]  {
-        if repeated < min {
-             print("M", key)
-            min = dicOfNumbs[num]!
-            key = num
-            print("K", key)
-        }
-        dicOfNumbs[num] = repeated + index
-        disrupt = num
-    }  else {
-        dicOfNumbs[num] = index
+    if let (value, _ ) = dicOfNumbs[num] {
+       dicOfNumbs[num]  = (value + index, true)
+       disrupt = num
+    } else {
+        dicOfNumbs[num] = (index, false)
     }
 }
-
 let remaining =  values.count - setOfNumbs.count
     
     if remaining == 0 {
@@ -40,10 +31,14 @@ let remaining =  values.count - setOfNumbs.count
         temp = disrupt
         return temp
     } else if remaining > 1 {
-        temp = key
-        print("T", key)
+        
+        for (key, (total, repeated)) in dicOfNumbs {
+            if total < min && repeated == true {
+                min = total
+                temp = key
+            }
+        }
     }
-    
     return temp
 }
 
